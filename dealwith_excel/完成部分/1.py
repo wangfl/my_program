19051827c91dd45a82dc1cd70365011906607844
wang fl <wangfl1989@163.com>
2015-09-10 14:45:22 +0800

    del 完成部分

--- dealwith_excel/完成部分/1.py
+++
@@ -1,8 +0,0 @@
-#__*__ coding:utf-8 __*__
-import os
-abs = os.path.abspath('.')  
-sourcedir = os.path.join(abs,'source')  
-sourcedir_xlutils = os.path.join(sourcedir,'xlutils-1.7.1')  
-#sourcedir_xlutilspy = os.path.join(sourcedir_xlutils,'setup.py install') 
-os.chdir(sourcedir_xlutils)
-os.system('cmd.exe')
